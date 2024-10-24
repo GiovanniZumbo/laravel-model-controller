f1458b728a821050dfd94b6c1b5280fb4623ed27
Giovanni Zumbo <gianni.zu07@gmail.com>
2024-10-24 17:18:23 +0200

    added cards basic layout

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -3,18 +3,25 @@
 @section('main-content')
     <div class="container">
 
-        <ul>
+        <div class="row row-cols-4 g-3">
             @forelse ($movies as $movie)
-                <li>
+                <div class="col">
 
-                    {{ $movie->title }}
+                    <div class="card h-100">
+                        <div class="card-body">
+                            <p> Title: "{{ $movie->title }}" </p>
+                            <p> Original title: "{{ $movie->original_title }}" </p>
+                            <p> National: {{ $movie->nationality }} </p>
+                            <p> Date: {{ $movie->date }} </p>
+                            <p> Vote: {{ $movie->vote }} </p>
+                        </div>
+                    </div>
 
-                </li>
+                </div>
             @empty
                 <p>Nessun film disponibile</p>
             @endforelse
-
-        </ul>
+        </div>
 
     </div>
 @endsection
